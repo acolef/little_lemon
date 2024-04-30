2d74ff809ca197958975f885fbc8ff67cf637d4d
Tuesday <a.cole.f@gmail.com>
2024-04-30 14:05:06 -0400

    Add semantic structure, a logo, and OGP meta tags

---
+++ components/Footer.js
@@ -0,0 +1,47 @@
+export const Footer = () => {
+    return (
+        <>
+            <section>
+                <h4>Doormat &br; Navigation</h4>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="index.html">Home</a>
+                        </li>
+                        <li>
+                            <a href="about.html">About</a>
+                        </li>
+                        <li>
+                            <a href="menu.html">Menu</a>
+                        </li>
+                        <li>
+                            <a href="reservations.html">Reservations</a>
+                        </li>
+                        <li>
+                            <a href="online-ordering.html">Order Online</a>
+                        </li>
+                        <li>
+                            <a href="login.html">Login</a>
+                        </li>
+                    </ul>
+                </nav>
+            </section>
+
+            <section>
+                <h4>Contact</h4>
+                <article>
+                    <p>Address</p>
+                    <p>Phone</p>
+                    <p>Email</p>
+                </article>
+            </section>
+
+            <section>
+                <h4>Social Media</h4>
+                <article>
+                    <p>Social media links here</p>
+                </article>
+            </section>
+        </>
+    );
+};
---
+++ components/Nav.js
@@ -0,0 +1,32 @@
+export const Nav = () => {
+    return (
+        <nav>
+            <img
+                src="assets/Logo.svg"
+                alt="Little Lemon restaurant logo"
+                width="202"
+                height="61"
+            />
+            <ul>
+                <li>
+                    <a href="index.html">Home</a>
+                </li>
+                <li>
+                    <a href="about.html">About</a>
+                </li>
+                <li>
+                    <a href="menu.html">Menu</a>
+                </li>
+                <li>
+                    <a href="reservations.html">Reservations</a>
+                </li>
+                <li>
+                    <a href="online-ordering.html">Order Online</a>
+                </li>
+                <li>
+                    <a href="login.html">Login</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
