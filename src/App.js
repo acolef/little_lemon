b5f03bfccec4879cfbfb2a37ecf1b9487717ede8
Tuesday <a.cole.f@gmail.com>
2024-05-02 14:38:36 -0400

    Added flexbox for footer, added a footer image, added content wrapper for page content, adjusted navbar flexbox rules

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Nav from './components/Nav';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-// import './App.css';
+import './App.css';
 
 function App() {
   return (
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,47 +1,64 @@
+import '../styles/Footer.css';
+import restaurant from '../assets/restaurant.jpg';
+
 const Footer = () => {
     return (
-        <>
-            <section>
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="index.html">Home</a>
-                        </li>
-                        <li>
-                            <a href="about.html">About</a>
-                        </li>
-                        <li>
-                            <a href="menu.html">Menu</a>
-                        </li>
-                        <li>
-                            <a href="reservations.html">Reservations</a>
-                        </li>
-                        <li>
-                            <a href="online-ordering.html">Order Online</a>
-                        </li>
-                        <li>
-                            <a href="login.html">Login</a>
-                        </li>
-                    </ul>
-                </nav>
-            </section>
+        <footer className="footer-section">
+            <div className="footer-elements content-wrapper">
+                <img
+                    src={restaurant}
+                    alt="An indoor photograph of the Little Lemon restaurant"
+                />
+                <div className="footer-links">
+                    <div className="footer-links-section">
+                        <section>
+                            <nav role="navigation">
+                                <ul>
+                                    <li>
+                                        <a href="index.html">Home</a>
+                                    </li>
+                                    <li>
+                                        <a href="about.html">About</a>
+                                    </li>
+                                    <li>
+                                        <a href="menu.html">Menu</a>
+                                    </li>
+                                    <li>
+                                        <a href="reservations.html">Reservations</a>
+                                    </li>
+                                    <li>
+                                        <a href="online-ordering.html">Order Online</a>
+                                    </li>
+                                    <li>
+                                        <a href="login.html">Login</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </section>
+                    </div>
 
-            <section>
-                <h4>Contact</h4>
-                <article>
-                    <p>Address</p>
-                    <p>Phone</p>
-                    <p>Email</p>
-                </article>
-            </section>
+                    <div className="footer-links-section">
+                        <section>
+                            <h4>Contact</h4>
+                            <article>
+                                <p>Address</p>
+                                <p>Phone</p>
+                                <p>Email</p>
+                            </article>
+                        </section>
+                    </div>
 
-            <section>
-                <h4>Social Media</h4>
-                <article>
-                    <p>Social media links here</p>
-                </article>
-            </section>
-        </>
+                    <div className="footer-links-section">
+                        <section>
+                            <h4>Social Media</h4>
+                            <article>
+                                <p>Social media links here</p>
+                            </article>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </footer>
     );
 };
 
